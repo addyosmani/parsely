e98c77b9842549885f059831ba69dc9d355978bc
addyosmani <addyosmani@gmail.com>
2011-10-17 04:20:27 +0100

    updating structure to parsely

--- parsely.js
+++ parsely.js
@@ -5,24 +5,37 @@
 * Copyright (c) 2011 Addy Osmani
 * Dual MIT/BSD license
 */
-function parsely( strUrl ){
-	var uri = {},
-		keys = ["source","protocol","authDomain","authLogin","user","password","host","port","relative","path","dir","file","query","anchor"],
-		i = keys.length,
-		n = /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/,
-		m = n.exec(strUrl),
-		ext = ['path','dir','anchor'],
-		c = ext.length
-		k = /(?:^|&)([^&=]*)=?([^&]*)/g, //for params &= etc.
-		l = /(?:[^\/\\]+|\\.)+/g; //for dirs/paths
 
-	while (i--) uri[keys[i]] = m[i] || "";
-	uri['queries'] = {};
-	uri['query'].replace(k, function ($0, $1, $2) {
-		if ($1) uri['queries'][$1] = $2;
-	});
+;(function ( window, doc, undef ) {
 
-	while(c--) uri[ext[c] + 's'] = uri[ext[c]].match(l) || "";
-	
-	return uri;
-}
+    parsely = function (strUrl) {
+        var uri = {},
+            keys = ["source", "protocol", "authDomain", "authLogin", "user", "password", "host", "port", "relative", "path", "dir", "file", "query", "anchor"],
+            i = keys.length,
+            n = /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/,
+            m = n.exec(strUrl),
+            ext = ['path', 'dir', 'anchor'],
+            c = ext.length
+            k = /(?:^|&)([^&=]*)=?([^&]*)/g,
+            //for params &= etc.
+            l = /(?:[^\/\\]+|\\.)+/g; //for dirs/paths
+        while (i--) uri[keys[i]] = m[i] || "";
+        uri['queries'] = {};
+        uri['query'].replace(k, function ($0, $1, $2) {
+            if ($1) uri['queries'][$1] = $2;
+        });
+
+        while (c--) uri[ext[c] + 's'] = uri[ext[c]].match(l) || "";
+
+        return uri;
+    }
+
+    getParsely = function () {
+        return parsely;
+    }
+
+    window.parsely = getParsely();
+
+})(this, this.document);
+
+
